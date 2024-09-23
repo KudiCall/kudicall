ae2a4ef00b375f578dba09b0f8c2952881f05d5a
Stellamaris Nwafor <wendumaris@gmail.com>
2024-09-23 19:44:59 +0100

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -6,4 +6,30 @@
 	</v-app>
 </template>
 
+<script setup lang="ts">
+const title = ref("My App");
+const description = ref("My App Description");
+
+// This will be reactive when you change title/description above
+useHead({
+	title,
+	meta: [
+		{
+			name: "description",
+			content: description,
+		},
+		{ property: "og:title", content: "OnCall" },
+		{ property: "og:description", content: "Discover a new way to Shop, Connect, and Experience!!" },
+		{ property: "og:image", content: "https://res.cloudinary.com/dd26v0ffw/image/upload/v1727115612/OnCall/favicon1_owh5zd.png" }, // Use absolute URL
+		{ property: "og:url", content: "https://www.oncall-shop.netlify.app" },
+		{ property: "og:type", content: "website" },
+		{ name: "twitter:card", content: "summary_large_image" },
+		{ name: "twitter:title", content: "OnCall" },
+		{ name: "twitter:description", content: "Discover a new way to Shop, Connect, and Experience!!" },
+		{ name: "twitter:image", content: "https://res.cloudinary.com/dd26v0ffw/image/upload/v1727115612/OnCall/favicon1_owh5zd.png" }, // Use absolute URL
+	],
+	link: [{ rel: "icon", type: "image/x-icon", href: "/favicon1.png" }],
+});
+</script>
+
 <style></style>
